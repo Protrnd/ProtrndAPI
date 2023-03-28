d7ce6a18155999b34cdacf50d8323bd90514b172
Kanderel <Jamesodike26@gmail.com>
2023-03-28 22:59:08 +0100

    new

--- Services/PaymentService.cs
+++ Services/PaymentService.cs
@@ -3,59 +3,112 @@ using MongoDB.Driver;
 using MongoDB.Driver.Linq;
 using PayStack.Net;
 using ProtrndWebAPI.Models.Payments;
-using ProtrndWebAPI.Models.User;
 using ProtrndWebAPI.Settings;
-using System.Security.Cryptography;
-using System.Text;
-using static MongoDB.Driver.WriteConcern;
-using AccountDetailsDTO = ProtrndWebAPI.Models.Payments.AccountDetailsDTO;
 
-namespace ProtrndWebAPI.Services
-{
-    public class PaymentService: BaseService
-    {
-        //private PayStackApi PayStack { get; set; }
-        public PaymentService(IOptions<DBSettings> settings) :base(settings)
+namespace ProtrndWebAPI.Services {
+    public class PaymentService : BaseService {
+        public PaymentService(IOptions<DBSettings> settings, IConfiguration configuration) : base(settings) { }
+
+        public async Task<AccountDetails?> AddAccountDetailsAsync(AccountDetailsDTO account)
         {
-            //PayStack = new(configuration["Payment:PaystackSK"]);
+            var accountExists = await GetAccountDetails(account.AccountNumber);
+            AccountDetails? accountDetails;
+            if (accountExists == null)
+            {
+                accountDetails = new AccountDetails { AccountNumber = account.AccountNumber, ProfileId = account.ProfileId };
+                await _accountDetailsCollection.InsertOneAsync(accountDetails);
+            }
+            else
+            {
+                accountDetails = accountExists;
+            }
+            if (accountDetails != null)
+                return accountDetails;
+            return null;
         }
 
-        //public async Task<AccountDetails?> AddAccountDetailsAsync(AccountDetailsDTO account, string token)
+        public async Task<bool> SupportAsync(Support support)
+        {
+            try
+            {
+                await _supportCollection.InsertOneAsync(support);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<List<Support>> GetAllSupports(Guid profileId)
+        {
+            return await _supportCollection.Find(s => s.ReceiverId == profileId).ToListAsync();
+        }
+
+        public async Task<List<Support>> GetSupportsOnPost(Guid postId)
+        {
+            return await _supportCollection.Find(s => s.PostId == postId).ToListAsync();
+        }
+
+        public async Task<int> GetSupportTotal(Guid profileId)
+        {
+            var supports = await GetAllSupports(profileId);
+            var total = 0;
+            foreach (var support in supports)
+            {
+                total += support.Amount;
+            }
+            return total;
+        }
+
+        public async Task<string> WithdrawSupports(Guid profileId)
+        {
+            var supportTotal = await GetSupportTotal(profileId);
+            if (supportTotal <= 0)
+            {
+                return "";
+            }
+            var support = new Support { Amount = -supportTotal, PostId = Guid.Empty, ReceiverId = profileId, SenderId = Guid.Empty, Reference = GenerateReference().ToString() };
+            await _supportCollection.InsertOneAsync(support);
+            return support.Reference;
+        }
+
+        private static int GenerateReference()
+        {
+            var r = new Random();
+            return r.Next(1000, 9999);
+        }
+
+        public async Task<AccountDetails?> GetAccountDetails(string accountnumber)
+        {
+            var details = await _accountDetailsCollection.Find(a => a.AccountNumber == accountnumber).FirstOrDefaultAsync();
+            return details;
+        }
+
+        //private static string EncryptDataWithAes(string plainText, string token)
         //{
-        //    var accountDetails = new AccountDetails { AuthCode = account.AuthCode, CardNumber = EncryptDataWithAes(account.CardNumber, token), CVV = EncryptDataWithAes(account.CVV, token), ExpirtyDate = EncryptDataWithAes(account.ExpirtyDate, token), ProfileId = account.ProfileId };
-        //    await _accountDetailsCollection.InsertOneAsync(accountDetails);
-        //    var filter = Builders<Profile>.Filter.Eq(p => p.Identifier, account.ProfileId);
-        //    var update = Builders<Profile>.Update.Set(s => s.AccountLinked, true);
-        //    var updateResult = await _profileCollection.FindOneAndUpdateAsync(filter, update);
-        //    if (accountDetails != null && updateResult != null)
-        //        return accountDetails;
-        //    return null;
+        //    byte[] inputArray = Encoding.UTF8.GetBytes(plainText);
+        //    var tripleDES = Aes.Create();
+        //    tripleDES.Key = Encoding.UTF8.GetBytes(token);
+        //    tripleDES.Mode = CipherMode.ECB;
+        //    tripleDES.Padding = PaddingMode.PKCS7;
+        //    ICryptoTransform cTransform = tripleDES.CreateEncryptor();
+        //    byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
+        //    tripleDES.Clear();
+        //    return Convert.ToBase64String(resultArray, 0, resultArray.Length);
         //}
 
-        private static string EncryptDataWithAes(string plainText, string token)
-        {
-            byte[] inputArray = Encoding.UTF8.GetBytes(plainText);
-            var tripleDES = Aes.Create();
-            tripleDES.Key = Encoding.UTF8.GetBytes(token);
-            tripleDES.Mode = CipherMode.ECB;
-            tripleDES.Padding = PaddingMode.PKCS7;
-            ICryptoTransform cTransform = tripleDES.CreateEncryptor();
-            byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
-            tripleDES.Clear();
-            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
-        }
-
-        private string DecryptDataWithAes(byte[] cipherText, string token)
-        {
-            var tripleDES = Aes.Create();
-            tripleDES.Key = Encoding.UTF8.GetBytes(token);
-            tripleDES.Mode = CipherMode.ECB;
-            tripleDES.Padding = PaddingMode.PKCS7;
-            ICryptoTransform cTransform = tripleDES.CreateDecryptor();
-            byte[] resultArray = cTransform.TransformFinalBlock(cipherText, 0, cipherText.Length);
-            tripleDES.Clear();
-            return Encoding.UTF8.GetString(resultArray);
-        }
+        //private string DecryptDataWithAes(byte[] cipherText, string token)
+        //{
+        //    var tripleDES = Aes.Create();
+        //    tripleDES.Key = Encoding.UTF8.GetBytes(token);
+        //    tripleDES.Mode = CipherMode.ECB;
+        //    tripleDES.Padding = PaddingMode.PKCS7;
+        //    ICryptoTransform cTransform = tripleDES.CreateDecryptor();
+        //    byte[] resultArray = cTransform.TransformFinalBlock(cipherText, 0, cipherText.Length);
+        //    tripleDES.Clear();
+        //    return Encoding.UTF8.GetString(resultArray);
+        //}
 
         public async Task<Transaction> GetTransactionByRefAsync(string reference)
         {
@@ -67,7 +120,6 @@ namespace ProtrndWebAPI.Services
             try
             {
                 transaction.Id = Guid.NewGuid();
-                transaction.Identifier = transaction.Id;
                 await _transactionCollection.InsertOneAsync(transaction);
                 return true;
             }
@@ -75,6 +127,12 @@ namespace ProtrndWebAPI.Services
             {
                 return false;
             }
+        }
+
+        public async Task<bool> TransactionExistsAsync(string reference)
+        {
+            var exists = await _transactionCollection.Find(t => t.TrxRef == reference).SingleOrDefaultAsync();
+            return exists != null;
         }
 
         public async Task<int> GetTotalBalance(Guid profileId)
@@ -89,101 +147,9 @@ namespace ProtrndWebAPI.Services
             return total;
         }
 
-        //public async Task<bool> ChargeAuthCode(Promotion promotion)
-        //{
-        //    var response = PayStack.Charge.ChargeAuthorizationCode(new AuthorizationCodeChargeRequest { AuthorizationCode = promotion.AuthCode, Email = promotion.Email, Amount = (promotion.Amount * 100).ToString() });
-        //    if (response.Data.Status == "success")
-        //        await UpdateNextPayDate(promotion);
-        //    else
-        //        await DisablePromotion(promotion);
-        //    return true;
-        //}
-
         public async Task<List<Promotion>> GetDuePromotions()
         {
-            return await _promotionCollection.Find(p => !p.Disabled && p.NextCharge == DateTime.Now || p.NextCharge.AddMinutes(1) == DateTime.Now).ToListAsync();
-        }
-
-        public async Task<bool> UpdateNextPayDate(Promotion promotion)
-        {
-            var interval = promotion.ChargeIntervals;
-            if (interval == "day")
-                promotion.NextCharge.AddDays(1);
-            if (interval == "week")
-                promotion.NextCharge.AddWeeks(1);
-            if (interval == "month")
-                promotion.NextCharge.AddMonths(1);
-            var filter = Builders<Promotion>.Filter.Where(p => !p.Disabled && p.ProfileId == promotion.ProfileId && p.Identifier == promotion.Identifier);
-            var updateSuccess = await _promotionCollection.ReplaceOneAsync(filter, promotion);
-            var transaction = new Transaction
-            {
-                Amount = promotion.Amount,
-                ProfileId = promotion.Identifier,
-                CreatedAt = DateTime.Now,
-                TrxRef = Generate().ToString(),
-                ItemId = promotion.PostId,
-                Purpose = $"Pay for promotion id = {promotion.PostId}"
-            };
-            await InsertTransactionAsync(transaction);
-            return updateSuccess.ModifiedCount > 0;
-        }
-
-        public async Task<bool> DisablePromotion(Promotion promotion)
-        {
-            var filter = Builders<Promotion>.Filter.Where(p => !p.Disabled && p.ProfileId == promotion.ProfileId && p.Identifier == promotion.Identifier);
-            promotion.Disabled = true;
-            var updateSuccess = await _promotionCollection.ReplaceOneAsync(filter, promotion);
-            var transaction = new Transaction
-            {
-                Amount = promotion.Amount,
-                ProfileId = promotion.Identifier,
-                CreatedAt = DateTime.Now,
-                TrxRef = Generate().ToString(),
-                ItemId = promotion.PostId,
-                Purpose = $"Payment failef for promotion id = {promotion.PostId}"
-            };
-            await InsertTransactionAsync(transaction);
-            return updateSuccess.ModifiedCount > 0;
-        }
-
-        public async Task<bool> RequestWithdrawalAsync(TokenClaims profile, int total)
-        {
-            // Modify withdrawal
-
-            int balance = await GetTotalBalance(profile.ID);
-            if (balance <= 100 || total < balance)
-                return false;
-
-            var transaction = new Transaction { Amount = -total, CreatedAt = DateTime.Now, ProfileId = profile.ID, Purpose = "Withdraw", TrxRef = Generate().ToString() };
-            await _transactionCollection.InsertOneAsync(transaction);
-            return true;
-        }
-
-        public async Task<int> GetTotalGiftsAsync(Guid profileId)
-        {
-            var gifts = await GetAllGiftAsync(profileId);
-            return gifts.Count;
-        }
-
-        public async Task<List<Gift>> GetAllGiftAsync(Guid profileId)
-        {
-            var filter = Builders<Gift>.Filter.Where(s => s.ProfileId == profileId && s.Disabled == false);
-            var gifts = await _giftsCollection.Find(filter).ToListAsync();
-            return gifts;
-        }
-
-        public async Task<bool> BuyGiftsAsync(Guid profileId, int count)
-        {
-            var gifts = Enumerable.Repeat(new Gift { Id = null, ProfileId = profileId, Disabled = false }, count);
-            try
-            {
-                await _giftsCollection.InsertManyAsync(gifts);
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
+            return await _promotionCollection.Find(p => !p.Disabled && p.ExpiryDate == DateTime.Now || p.ExpiryDate.AddMinutes(1) == DateTime.Now).ToListAsync();
         }
     }
 }
