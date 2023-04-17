0f2373cc516b5432b8dcd8ac1e024f16b2e81cc7
Kanderel <Jamesodike26@gmail.com>
2023-04-17 14:12:20 +0100

    add account details when withdrawing and send mail

--- Models/User/AccountDetailsDTO.cs
+++ Models/User/AccountDetailsDTO.cs
@@ -1,10 +1,16 @@
-﻿namespace ProtrndWebAPI.Models.User
+﻿using System.Text.Json.Serialization;
+
+namespace ProtrndWebAPI.Models.User
 {
     public class AccountDetailsDTO
     {
+        [JsonPropertyName("accountnumber")]
         public string AccountNumber { set; get; } = string.Empty;
+        [JsonPropertyName("profileid")]
         public Guid ProfileId { get; set; } = Guid.Empty;
+        [JsonPropertyName("bankname")]
         public string BankName { get; set; } = string.Empty;
+        [JsonPropertyName("accountname")]
         public string AccountName { get; set; } = string.Empty;
     }
 }
